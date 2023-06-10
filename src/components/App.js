5e429f9b8ff42aa7e86328392ee2ff7666c0d982
Arjun Singh <arjunsinghbazal@gmail.com>
2023-06-10 18:56:43 +0530

    submitting code-Sat Jun 10 2023 18:56:42 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,26 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
-
+import Tab from "./Tab";
+const Head=(prop)=>{
+return (
+  <Tab title={prop}/>
+)
+}
 const App = () => {
+  [current,setCurrent]=useState("");
+  const update=(that)=>{
+    return setCurrent(that);
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+     <ul>
+      <li onClick={update("Tab 1")}>Tab 1</li>
+      <li onClick={update('Tab 2')}>Tab 2</li>
+      <li onClick={update('Tab 3')}>Tab 3</li>
+
+     </ul>
+     <p><Tab title={current}/></p>
     </div>
   )
 }
---
+++ src/components/Tab.js
@@ -0,0 +1,6 @@
+const Tab=(props)=>{
+    return(
+        <p>This is content for {props.title}</p>
+    )};
+
+export default Tab;
